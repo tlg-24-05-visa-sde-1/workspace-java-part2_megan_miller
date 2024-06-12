59e24fcd461b1b3d0e9fd9c88761af5211e448c7
Megan Miller <MeganM@tlgcohort.com>
2024-06-12 09:50:51 -0500

    New switch expression in TaxCalculatorFactory

--- Lab05.3/src/com/javatunes/billing/TaxCalculatorFactory.java
+++ Lab05.3/src/com/javatunes/billing/TaxCalculatorFactory.java
@@ -3,18 +3,38 @@ package com.javatunes.billing;
 public class TaxCalculatorFactory {
 
     public static TaxCalculator getTaxCalculator(Location location) {
-        TaxCalculator calc = null;
-
-        switch(location) {
-            case ONLINE:
-                calc = new OnlineTax();
-                break;
-            case USA:
-                calc = new USATax();
-                break;
-            case EUROPE:
-                calc = new EuropeTax();
-        }
-        return calc;
+        return switch (location) {
+            case ONLINE -> new OnlineTax();
+            case USA -> new USATax();
+            case EUROPE -> new EuropeTax();
+        };
     }
 }
+
+
+//        switch (location) {  // using the new switch, early return version. BC we return early, no need for breaks.
+//            case ONLINE:
+//                return new OnlineTax();
+//            case USA:
+//                return new USATax();
+//            case EUROPE:
+//                return new EuropeTax();
+//        }
+//        return null; // have to use return null at the end with this.
+//    }
+//
+//        switch (location) {
+//            case ONLINE:
+//                calc = new OnlineTax();
+//                break;
+//            case USA:
+//                calc = new USATax();
+//                break;
+//            case EUROPE:
+//                calc = new EuropeTax();
+//
+//        }
+//        return calc;
+//        }
+
+
